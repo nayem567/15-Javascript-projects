0ffd6240cb7207fb2c43a1c25f746ffdbfbadd11
nayem567 <inew2732@gmail.com>
2024-02-19 21:39:11 +0600

    0.2

---
+++ 02-counter 19-2-2024/app.js
@@ -0,0 +1,41 @@
+
+// let number = document.querySelector('#value');
+// const increase_button = document.querySelector('.increase');
+// const reset_button = document.querySelector('.reset');
+// const decrease_button = document.querySelector('.decrease');
+
+
+// increase_button.addEventListener('click', function(){
+//     // "+" sign coverts string to a number
+//     number.textContent = +number.textContent + 1;
+//     updateColor();
+// });
+
+// decrease_button.addEventListener('click', function(){
+//     number.textContent = +number.textContent - 1;
+//     updateColor();
+// });
+
+// reset_button.addEventListener('click', function(){
+//     number.textContent = 0;
+//     updateColor();
+// });
+
+// function updateColor(){
+    
+//     // convert text content to a number
+//     let currentNumber = parseInt(number.textContent);
+
+//    if(currentNumber > 0){
+//     number.style.color = 'green';
+//    }else if(currentNumber < 0){
+//     number.style.color = 'red';
+//    }else{
+//     number.style.color = '#102a42';
+//    }
+// }
+
+
+// =======================================================
+
+
